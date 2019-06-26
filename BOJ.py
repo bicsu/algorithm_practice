98e5d9b78d938e989c489593674967d783ea4556
Kwansu Kim <42540640+bicsu@users.noreply.github.com>
2019-06-26 10:30:55 +0900

    0626

--- BOJ.py
+++ BOJ.py
@@ -1,30 +1,12 @@
-try : 
-	arr = list(map(int, input().split()))
-	n = arr[0]
-	k = arr[1]
-	ns = list(range(1,n+1))
-	dap = []
-	cursor = 0
-	#k = k-1
+#17173
 
-	while ns !=[]:
-	    if cursor + (k-1) <= len(ns)-1:
-	        cursor += (k-1)
-	        dap.append(ns.pop(cursor))
-	    elif len(ns) == 1:
-	        dap.append(ns.pop())
-	    else : 
-	        if cursor+k-1 >= len(ns)*2:
-	            cursor -= len(ns)*(((cursor+k-1)//len(ns))-1)
-	        cursor = cursor + k-1 - len(ns)
-	        dap.append(ns.pop(cursor))
-	st = '<'
-	for i in dap:
-		if i == dap[-1]:
-			st = st+(str(i))
-		else : 
-			st = st+(str(i)+', ')
-
-	print(st+'>')
-except :
-	exit()
+n, m = map(int, input().split())
+k_list = list(map(int, input().split()))
+n_list = list(range(1,n+1))
+dap = []
+for i in k_list:
+	for j in n_list:
+		if i * j in n_list:
+			dap.append(i*j)
+dap = set(dap)
+print(sum(list(dap)))
---
+++ BOJ_2774.py
@@ -0,0 +1,11 @@
+#2774
+
+t = int(input())
+
+for i in range(t):
+	x = list(input())
+
+	x = set(x)
+	print(len(x))
+
+
